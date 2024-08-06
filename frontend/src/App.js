329f24208c5ca6731340531c77afd253c907c8af
web-ghost-thiru <thirumurugankutty@gmail.com>
2024-08-06 12:25:03 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header/Header";
+
+function App() {
+  return (
+    <>
+    <BrowserRouter>
+    <Header />
+    </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
